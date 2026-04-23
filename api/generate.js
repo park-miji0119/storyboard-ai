67663358ebf193587f39cffe089125102ff5ba8f
park-miji0119 <onlyvn1@gmail.com>
2026-04-23 11:54:09 +0900

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+export const config = { runtime: 'nodejs' };export default async function handler(req, res) {
   // CORS 허용
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
